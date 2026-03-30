293d63b23f0b0e9be76db52e4b85206e0ceb047f
Alex <ahuberde@my.centennialcollege.ca>
2026-03-29 21:35:05 -0500

    restarting as nothing works

--- lab8/Source/lab8/lab8.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "lab8.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, lab8, "lab8" );
-
-DEFINE_LOG_CATEGORY(Loglab8)
--- lab8/Source/lab8/lab8GameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "lab8GameMode.h"
-
-Alab8GameMode::Alab8GameMode()
-{
-	// stub
-}
